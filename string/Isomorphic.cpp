09d3d6460a064d6249b949dcdce2d5aae4db7711
PALLAB GHOSH <gpallab405@gmail.com>
2023-03-21 19:29:45 +0530

    isomorphic_string_leetcode

---
+++ string/Isomorphic.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include<string.h>
+#include<unordered_map>
+using namespace std;
+
+
+  bool isIsomorphic(string s, string t) {
+      unordered_map <char,int> mp1,mp2;
+        int n=s.size();
+        for(int i=0;i<n;i++){
+            if(mp1[s[i]]!=mp2[t[i]]) 
+                return false;
+                
+            mp1[s[i]]=i+1;
+            mp2[t[i]]=i+1;
+        }
+        return true;
+    }
+int main(int argc, char** argv) {
+	string s="foor";
+	string t="bar";
+	if(isIsomorphic(s,t))
+	{
+		cout<<"yess";
+	}
+	else
+	{
+		cout<<"not";
+	}
+	
+
+}
